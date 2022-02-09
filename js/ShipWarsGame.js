9e837aef4f089d5d1a8a44b58835cfbabe87ed29
AurelienRoge <aurelien.roge@student.yncrea.fr>
2022-02-09 16:09:51 +0100

    game 33%

--- js/ShipWarsGame.js
+++ js/ShipWarsGame.js
@@ -1,21 +1,75 @@
 class shipWarsGame{
     constructor(){
-        this.TilesTab =  new Array()
+
+        //Initialisation du tableau représentant la carte du jeu.
+        //Tableau de 100 cases (10x10) de caractères : V pour Vide, B pour bateau, T pour Touché, R pour raté
+        this.GameMapPlayer0 =  new Array(100)
         for(let i = 0; i < 100; i++){
-            this.TilesTab.push(document.getElementsByClassName("gametile")[i]);
+            this.GameMapPlayer0[i] = "V";
         }
-        console.log(this.TilesTab)
-        this.EventListeners();
+
+        this.GameMapPlayer1 =  new Array(100)
+        for(let i = 0; i < 100; i++){
+            this.GameMapPlayer1[i] = "V";
+        }
+        
+
+
+        this.playerTurn = 0;
+
+        this.initializeRandomMap(this.GameMapPlayer0);
+        this.initializeRandomMap(this.GameMapPlayer1);
+        console.log(this.GameMapPlayer0);
+        console.log(this.GameMapPlayer1);
+       
     }
 
-    EventListeners(){
+
+    initializeRandomMap(GameMap){
+        for(let i = 0; i < GameMap.length; i++){
+            if(Math.floor(Math.random() * 100) % 5 == 0){
+                GameMap[i] = "B"
+            }
+        }
+    }
+
+    playerAttack(index, GameMap){
+        if(GameMap[index] != "R" && GameMap[index] != "T"){
+            switch(GameMap[index]){
+                case "B":
+                    GameMap[index] = "T";
+                    break;
+                case "V":
+                    GameMap[index] = "R";
+                    this.changePlayerTurn();
+            }
+        }
+        else{
+            console.log("Tir impossible sur cette case !");
+        }
+    }
+
+    changePlayerTurn(){
+        if(this.playerTurn == 0){
+            this.playerTurn = 1
+        }
+        else{
+            this.playerTurn = 0;
+        }
+    }
+}
+
+
+
+
+
+/*shipWarsGame.prototype.tileOnClickEvent = function(){
+    console.log(self);
+}
+
+EventListeners(){
         for(let i = 0; i < this.TilesTab.length; i++){
             this.TilesTab[i].addEventListener('click', this.tileOnClickEvent.bind(this.TilesTab[i]));
         }
     }
-}
-
-
-shipWarsGame.prototype.tileOnClickEvent = function(){
-    console.log(self);
-}
+ Pour la partie view */
